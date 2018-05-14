1448d4367b0a6dc4a4b13aae69c8dcdf69e105ec
JuliFed <fedorchenkojuli@gmail.com>
2018-05-14 15:27:56 +0300

    Writing classes for army and other

--- unit.py
+++ unit.py
@@ -1,32 +1,103 @@
 import random
+from base import Unit
+
+try:
+    import numpy as np
+except ImportError:
+    print("Can't import numpy")
+
+ROUND_NUMBER = 2
 
 
-class Unit:
-	def __init__(self):
-		self.health = 100
-		self.recharge = random.randrange(100, 2000)		
+def geo_mean(iterable):
+    a = np.array(iterable)
+    return a.prod() ** (1.0 / len(a))
 
-		
-class	Soldier(Unit):
-	def __init__(self):
-		self.experience = 0
-	
-	""" 
-		Вероятность успеха атаки
-	"""
-	@property
-	def attack_probability(self):
-		# 0.5 * (1 + health/100) * random(50 + experience, 100) / 100
-		return 0.5 * (1 + self.health/100) * random.randrange(50+self.experience, 100) / 100
-	
-	
-	""" 
-		Сумма нанесенного урона
-	"""
-	@property
-	def damage(self):
-		# 0.05 + experience / 100
-		return 0.05 + self.experience / 100
-	
-	
-	
+
+class Soldier(Unit):
+    experience = 0
+
+    def __init__(self, experience=0):
+        self.experience = experience
+
+    """ 
+        Р’РµСЂРѕСЏС‚РЅРѕСЃС‚СЊ СѓСЃРїРµС…Р° Р°С‚Р°РєРё
+    """
+
+    def attack(self):
+        # 0.5 * (1 + health/100) * random(50 + experience, 100) / 100
+        return round(0.5 * (1 + self.health / 100) * random.randrange(50 + self.experience, 100) / 100, ROUND_NUMBER)
+
+    """
+        РЎСѓРјРјР° РЅР°РЅРµСЃРµРЅРЅРѕРіРѕ СѓСЂРѕРЅР°
+    """
+
+    def damage(self):
+        # 0.05 + experience / 100
+        return round(0.05 + self.experience / 100, ROUND_NUMBER)
+
+    def add_experience(self):
+        if self.experience <= 50:
+            self.experience += 1
+
+
+class Vehicles(Unit):
+    """
+
+    """
+    operators = []
+
+    def __init__(self, unit_name=Soldier, count_operators=3):
+        self.count_operators = count_operators
+        for i in range(count_operators):
+            self.operators.append(unit_name())
+        self.health = 100
+        self.recharge = random.randrange(1000, 2000)
+
+    def total_health(self):
+        total_health = self.health
+        for unit in self.operators:
+            total_health += unit.health
+
+        return round(total_health / (self.count_operators + 1), ROUND_NUMBER)
+
+    def attack(self):
+        attacks_value = [i.attack
+                         for i in self.operators
+                         ]
+        return round(0.5 * (1 + self.health / 100) * geo_mean(attacks_value), ROUND_NUMBER)
+
+    def operators_experience(self):
+        sum_exp = 0
+        for i in self.operators:
+            sum_exp += i.experience / 100
+        return sum_exp
+
+    def damage(self):
+        return round(0.1 + self.operators_experience(), ROUND_NUMBER)
+
+
+class Squads:
+    units = []
+    count_units = 0
+
+    def add_units(self,count_units, unit_type=Soldier):
+        print(unit_type, count_units)
+        for i in range(count_units):
+            self.units.append(unit_type())
+        self.count_units += count_units
+
+    def damage(self):
+        total_damage = 0
+        for unit in self.units:
+            total_damage += unit.damage
+        return total_damage
+
+
+class Army:
+    strategy = None
+
+    def __init__(self, strategy):
+        self.strategy = strategy
+
+
